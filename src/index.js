1938d2e30e9f18b3d6fd6f237b3ba9d21ce0b526
Yuri <zhukovscy0@mail.ru>
2019-03-23 19:51:16 +0300

    complete brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
+  var stack = [];
+      var open = [];
+      var close = [];
+
+      for (i = 0; i < bracketsConfig.length; i++) {
+      open.push(bracketsConfig[i][0]);
+      close.push(bracketsConfig[i][1]);
+      }
+
+      for (i = 0; i < str.length; i++) {
+      var last = stack[stack.length - 1];
+      if (open.indexOf(last) !== close.indexOf(str[i]) || stack.length === 0) 
+      
+      {
+      stack.push(str[i]);
+      } else if (open.indexOf(last) === close.indexOf(str[i]) && close.indexOf(str[i]) !== -1 ) {
+      stack.pop();
+      
+      } else {
+      return false;
+      }
+      }
+      if (stack.length) {
+      return false;
+      }
+ 	return true;
   // your solution
 }
